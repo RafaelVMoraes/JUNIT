e6712038c9371c0992bf547c354be5babcbb299c
RafaelVMoraes <80759332+RafaelVMoraes@users.noreply.github.com>
2023-01-05 10:59:45 +0100

    pom change to add sonarcloud

--- src/main/java/org/example/Utils.java
+++ src/main/java/org/example/Utils.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package sample;
+package org.example;
 
 import java.lang.reflect.Method;
 import java.math.BigInteger;
--- src/main/java/org/example/Vectors.java
+++ src/main/java/org/example/Vectors.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package sample;
+package org.example;
 /**
  * Sample utility class for vector algebra.
  *
